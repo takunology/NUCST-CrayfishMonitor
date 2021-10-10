a7ce34b99e0676b0681df7d32f9c00437b530298
たくのろじぃ / Takunology <takunology.net@gmail.com>
2021-10-11 01:18:55 +0900

    Update AD_Converter.ino

--- Arduino/AD_Converter/AD_Converter.ino
+++ Arduino/AD_Converter/AD_Converter.ino
@@ -1,11 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
+  pinMode(13, OUTPUT );
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int analogVal = 0;
+  digitalWrite(13, HIGH);
   
   analogVal = analogRead(A0);
   float digitalVal = (float)(5 * analogVal)/1024;
@@ -13,5 +15,5 @@ void loop() {
   Serial.print(",");
   Serial.println(String(digitalVal, 3));
   
-  delay(100);
+  delay(10);
 }
