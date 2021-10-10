329e49e3d02667817783f812a945c60f94cde86f
たくのろじぃ / Takunology <takunology.net@gmail.com>
2021-10-11 03:45:10 +0900

    Make WPF Project

---
+++ ArduinoApps/WPFApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Reactive.Bindings;
+
+namespace WPFApp.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        
+    }
+}
