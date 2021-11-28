b372f78add4f5f62d26a8be0de3cb550556cf673
たくのろじぃ / Takunology <takunology.net@gmail.com>
2021-11-28 14:24:28 +0900

    Modified Arduino code

--- Arduino/AD_Converter_ADS1115/AD_Converter_ADS1115.ino
+++ Arduino/AD_Converter_ADS1115/AD_Converter_ADS1115.ino
@@ -4,8 +4,10 @@ Adafruit_ADS1115 ads;
 
 void setup() {
   Serial.begin(9600);
-  //+-4.096V
-  ads.setGain(GAIN_ONE);
+  
+   ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 0.0625mV
   if (!ads.begin()) {
     Serial.println("Failed to initialize ADS.");
     while (1);
@@ -13,17 +15,22 @@ void setup() {
 }
 
 void loop() {
-  int16_t input_0;
-  double voltage;
-  
-  input_0 = ads.readADC_Differential_0_1();
+  int16_t input_0 = 0;
+  float voltage = 0;
 
-  //+-4.096Vのときは0.125 / +-6.144Vのとき0.1875
-  voltage = (double)(input_0 * 0.125)/1000;
+  //平均化するか？
+  /*for (int i = 0; i < 5; i++){
+    input_0 = ads.readADC_SingleEnded(0);
+    voltage += ads.computeVolts(input_0);
+  }
+  
+  voltage = voltage / 10;
+  */
+  input_0 = ads.readADC_SingleEnded(0);
+  voltage += ads.computeVolts(input_0);
+  //Serial.print(input_0);
+  //Serial.print("\t");
   Serial.println(String(voltage, 5));
   
-  /*Serial.print(input_0);
-  Serial.print(" ");
-  Serial.println(voltage);*/
-  delay(5);
+  //delay(5);
 }
