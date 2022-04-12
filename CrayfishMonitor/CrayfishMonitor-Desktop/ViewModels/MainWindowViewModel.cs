9e2e28037e57f94d2008f44c3979a9525cde8a73
takunology <takunology.net@gmail.com>
2022-04-12 19:49:48 +0900

    画面遷移の実装

---
+++ CrayfishMonitor/CrayfishMonitor-Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CrayfishMonitor_Desktop.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ReactivePropertySlim<Page> PageContent { get; set; } = new ReactivePropertySlim<Page>(new Views.MonitorPage());
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
---
+++ CrayfishMonitor/CrayfishMonitor-Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace CrayfishMonitor_Desktop.Views
+{
+    public sealed partial class MainWindow : Window
+    {
+        ViewModels.MainWindowViewModel ViewModel { get; } = new ViewModels.MainWindowViewModel();
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            _ = args.SelectedItemContainer.Tag.ToString() switch
+            {
+                "MonitorPage" => frame.Navigate(typeof(Views.MonitorPage)),
+                "DataPage" => frame.Navigate(typeof(Views.DataPage)),
+                _ => frame.Navigate(typeof(Views.MonitorPage))
+            };
+            if (args.IsSettingsSelected) frame.Navigate(typeof(Views.SettingsPage));
+        }
+    }
+}
